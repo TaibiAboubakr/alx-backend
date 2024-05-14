1acef614e59d581728edb998d21eefdba0939147
aboubakr <c.aboubakr@gmail.com>
2024-05-14 18:24:56 +0100

    update the file 2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -27,7 +27,7 @@ def get_locale():
     user = getattr(g, 'user', None)
     if user is not None:
         return user.locale
-    return request.accept_languages.best_match(['en','fr','de'])
+    return request.accept_languages.best_match(['en','fr'])
 
 
 @app.route('/')
