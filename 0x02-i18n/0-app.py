8e0ecfdcfdac4c1af93b62ae113e9a172f463f57
aboubakr <c.aboubakr@gmail.com>
2024-05-14 17:45:07 +0100

    add the file 0-app.py

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" app """
+
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+app.route('/')
+def home():
+    """ home page """
+    return render_template('0-index.html')
