f1ede182705883cdc05e79d32c008050f33e028e
aboubakr <c.aboubakr@gmail.com>
2024-05-14 18:26:10 +0100

    update the file 2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -24,7 +24,7 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale():
     """if a user is logged in, use the locale from the user settings"""
-    return request.accept_languages.best_match(['en','fr'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
