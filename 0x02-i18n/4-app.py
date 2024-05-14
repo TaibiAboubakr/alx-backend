1414f7c612fb5dc680f25471a0ab7be336595841
aboubakr <c.aboubakr@gmail.com>
2024-05-14 18:50:49 +0100

    add the file 4-app.py

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+""" app """
+
+
+from flask import Flask
+from flask_babel import Babel
+from flask import render_template
+from flask import g, request
+
+
+class Config(object):
+    """ config class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = LANGUAGES[0]
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """if a user is logged in, use the locale from the user settings"""
+    lang = request.args.get('locale')
+    if lang in app.config['LANGUAGES']:
+        return lang
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def home():
+    """ home page """
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
