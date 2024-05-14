ffc8bb702f58552ecada26a275d7f8fcc8d98ad5
aboubakr <c.aboubakr@gmail.com>
2024-05-14 18:02:38 +0100

    update the file 1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -5,18 +5,18 @@
 from flask import Flask
 from flask_babel import Babel
 from flask import render_template
-app = Flask(__name__)
-babel = Babel(app)
 
 
-""" class config """
-
-
-class Config():
+class Config(object):
     """ config class """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = LANGUAGES[0]
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
 
 
 @app.route('/')
