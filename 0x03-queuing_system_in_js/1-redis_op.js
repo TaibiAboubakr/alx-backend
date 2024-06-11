867b6fff32e9e4a9a78555f4e7a5a408ec484efe
aboubakr <c.aboubakr@gmail.com>
2024-06-12 00:26:00 +0100

    add the file 1-redis_op.js

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+// 1-redis_op.js
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+// Function to set a new school in Redis
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+// Function to display the value of a school in Redis
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, reply) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(reply);
+    }
+  });
+}
+
+// Example usage
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
